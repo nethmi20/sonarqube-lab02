9113f85cdd6c6c7958adc2c9f9436b1741d75d7a
nethmi20 <142481124+nethmi20@users.noreply.github.com>
2026-01-27 07:07:49 +0530

    New issues

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,35 +1,36 @@
 package main.java.com.example;
 
-public int calculate(int a, int b, String op) {
+public class Calculator {
 
-    if(op.equals("add")) {
-        return a + b;
-    } else if(op.equals("add-again")) {
-        return a + b; // DUPLICATION
-    } else if(op.equals("sub")) {
-        return a - b;
-    } else if(op.equals("sub-again")) {
-        return a - b; // DUPLICATION
-    } else if(op.equals("mul")) {
-        return a * b;
-    } else if(op.equals("div")) {
-        if(b == 0) {
-            return 0;
+    public int calculate(int a, int b, String op) {
+        if(op.equals("add")) {
+            return a + b;
+        } else if(op.equals("add-again")) {
+            return a + b; // DUPLICATION
+        } else if(op.equals("sub")) {
+            return a - b;
+        } else if(op.equals("sub-again")) {
+            return a - b; // DUPLICATION
+        } else if(op.equals("mul")) {
+            return a * b;
+        } else if(op.equals("div")) {
+            if(b == 0) {
+                return 0;
+            } else {
+                return a / b;
+            }
+        } else if(op.equals("mod")) {
+            return a % b;
+        } else if(op.equals("pow")) {
+            int result = 1;
+            for(int i = 0; i < b; i++) {
+                result = result * a;
+            }
+            return result;
         } else {
-            return a / b;
+            return 0;
         }
-    } else if(op.equals("mod")) {
-        return a % b;
-    } else if(op.equals("pow")) {
-        int result = 1;
-        for(int i = 0; i < b; i++) {
-            result = result * a;
-        }
-        return result;
-    } else {
-        return 0;
     }
-}
 
     // Code Duplication (students must remove)
     public int addNumbers(int x, int y) {
